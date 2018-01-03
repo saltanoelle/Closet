96cf35148b0d999a4d7a9e04ba22d7d2a71ca7e8
noelle <saltanoelle@gmail.com>
2018-01-02 19:35:16 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+
+    
+  render() {
+    return (
+      
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">My Closet</h1>
+        </header>
+        <p className="App-intro">
+        Join now!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
